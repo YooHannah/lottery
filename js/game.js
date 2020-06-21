d12c0112769a179aa99f2693ccf5ef1214062122
YooHannah <1942063705@qq.com>
2020-06-21 22:10:15 +0800

    a little game

---
+++ js/game.js
@@ -0,0 +1,82 @@
+import dom from "./dom.js";
+import util from "./util.js";
+class Game{
+  constructor(options){
+    this.defaultPrize = [
+      '华为',
+      'iPhoneX',
+      '谢谢惠顾',
+      '手环',
+      '小熊抱枕',
+      '格力冰箱',
+      '电风扇',
+      '小度音响'
+    ]
+    this.prize = options.prize
+    this.timer = false
+    this.circus = options.circus?options.circus:56 //定制转的圈数，8的整数倍
+    this.game_pane = util.generateTemplate(this.defaultPrize,this.prize)
+    this.initStartUI(); // 初始化开始界面
+  }
+  /**
+   * 初始化游戏界面
+   */
+  initStartUI() { 
+    let el = dom.create(this.game_pane);
+    let btn = el.querySelector(".btn");
+    dom.insert(el);
+    btn.addEventListener("click", e => {
+      this.start();
+    });
+  }
+  start(){
+    //阻止抽奖过程随意点击
+    if(this.timer){
+      return
+    }
+    this.timer = true
+    let rand =util.getPrize(this.prize)+this.circus
+    let sort = [1,2,3,4,6,7,8,9]
+    let doms = dom.getElements('.prize')
+    let i = 0
+    let count = 0
+    let timer = null
+    let timerOperation = ()=>{
+      let prize = ''
+      for(let j = 0;j<doms.length;j++){
+        let element = doms[j]
+        if(element.dataset.index){
+          element.className = 'prize'
+        }
+        if(element.dataset.index === String(sort[i])){
+          element.className = 'prize active'
+          prize = element.innerHTML
+        }
+      }
+      if(i === 7){i=-1}
+      let speed = parseInt(this.circus/3)
+      if(count>speed){
+        clearInterval(timer)
+        timer = setInterval(timerOperation,100)
+      }
+      if(count>this.circus-11){
+        clearInterval(timer)
+        timer = setInterval(timerOperation,300)
+      }
+      if(count === rand){
+        let tempFunc = ()=>{
+          this.timer = false
+          alert('你获得的奖品是：'+prize)
+       }
+        setTimeout(tempFunc,0)
+        clearInterval(timer)
+        return
+      }
+      count++
+      i++
+    }
+    timer = setInterval(timerOperation,300)
+  }
+}
+
+export default Game;
---
+++ js/util.js
@@ -0,0 +1,61 @@
+export default {
+  prizeArr:[],
+  prizeObj:{},
+  generatePrize(defaultPrize,prize){
+    let customPrize=prize?Object.keys(prize):defaultPrize
+    let prizes = prize?Object.assign({},prize):{}
+    if(customPrize.length<8){ //用默认奖品补全8个奖品
+      for(let i = 0;i<defaultPrize.length;i++){
+        if(!customPrize.includes(defaultPrize[i])){
+          customPrize.push(defaultPrize[i])
+          prizes[defaultPrize[i]] = 1000
+        }
+        if(customPrize.length===8){break}
+      }
+    }
+    this.prizeArr = customPrize
+    this.prizeObj = prizes
+    return {
+      prizeArr:customPrize,
+      prizeObj:prizes
+    }
+  },
+  //生成HTML 旋转顺序和数组顺序相同
+  generateTemplate(defaultPrize,prize){
+    let prizes = this.generatePrize(defaultPrize,prize).prizeArr
+    let str = ''
+    str = `<div class='prize' data-index='1'>${prizes[0]}</div>
+    <div class='prize' data-index='2'>${prizes[1]}</div>
+    <div class='prize' data-index='3'>${prizes[2]}</div>
+    <div class='prize' data-index='9'>${prizes[7]}</div>
+    <div class='prize btn' data-index='5'>开始</div>
+    <div class='prize' data-index='4'>${prizes[3]}</div>
+    <div class='prize' data-index='8'>${prizes[6]}</div>
+    <div class='prize' data-index='7'>${prizes[5]}</div>
+    <div class='prize' data-index='6'>${prizes[4]}</div>`
+    return `'<div class="game_pane">${str}</div>'`
+  },
+  //计算奖品
+  getPrize(prize){
+    let rand=parseInt(Math.random()*100)
+    let nums =Object.values(prize)
+    let probTotal = 0
+    if(nums.length<8){
+      let len = 8-nums.length
+      let temp = nums.reduce((item,i)=>item+i,0)
+      let prob = (100-temp)/len
+      for(let k =0;k<len;k++){
+        nums.push(prob)
+      }
+    }
+    let index = 0
+    for(let i = 0;i<nums.length;i++){
+      probTotal+=nums[i]
+      if(rand<probTotal){
+        index = i
+        break
+      }
+    }
+    return index
+  }
+}
